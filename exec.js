479f1abdd6d0c259b5314cacbecb424d4a7265c8
jopemachine <gravityc95@gmail.com>
2020-10-07 21:50:41 +0900

    Update exec.js

--- exec.js
+++ exec.js
@@ -1,28 +1,27 @@
 #!/usr/bin/env node
-const path = require('path');
-const recursive = require("recursive-readdir");
+const path = require('path')
+const recursive = require('recursive-readdir')
 
 module.exports = (type, commandArguments) => {
-  const csvConvert = require("./csvConvert");
+  const csvConvert = require('./csvConvert')
 
-  if (commandArguments["dir"]) {
-    recursive(path.resolve(commandArguments["src"]), [], (err, files) => {
+  if (commandArguments.dir) {
+    recursive(path.resolve(commandArguments.src), [], (_err, files) => {
       const csvFiles = files.map(filePath => {
-        const targetFileName = filePath.split(path.sep).reverse()[0];
-        if (targetFileName.split(".")[1] === 'csv') return filePath;
-      });
+        const targetFileName = filePath.split(path.sep).reverse()[0]
+        if (targetFileName.split('.')[1] === 'csv') return filePath
+      })
 
-      let loopCnt = 1;
-      for (let csvFilePath of csvFiles) {
-        if (!csvFilePath) continue;
-        commandArguments["src"] = csvFilePath;
-        console.log(`Processing ${loopCnt++}th file..`);
-        csvConvert(type, commandArguments);
+      let loopCnt = 1
+      for (const csvFilePath of csvFiles) {
+        if (!csvFilePath) continue
+        commandArguments.src = csvFilePath
+        console.log(`Processing ${loopCnt++}th file..`)
+        csvConvert(type, commandArguments)
       }
-      console.log("Jobs done.");
+      console.log('Jobs done.')
     })
+  } else {
+    csvConvert(type, commandArguments)
   }
-  else {
-    csvConvert(type, commandArguments);
-  }
-};
+}
